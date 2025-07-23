08e0e4cb49a2f1171aa585da8b744372f9f357d1
LFranklinApps <lfranklinapps@gmail.com>
2025-07-22 20:24:07 -0500

    Update Custom_Robot_Control.cpp

--- Custom_Robot_Control.cpp
+++ Custom_Robot_Control.cpp
@@ -2,6 +2,9 @@
   Custom_Robot_Control.cpp - Library for communicating with the Custom Robot Control app over Bluetooth.
   Created by L. Franklin, July 15, 2025.
   Updated by L. Franklin, July 15, 2025.
+
+  Notes:
+  "Android is always little-endian" (https://developer.android.com/ndk/guides/abis)
 */
 
 #include "Arduino.h"
@@ -9,7 +12,7 @@
 #include "Custom_Robot_Control.h"
 
 BLEService mainService("F84ECD8E-FC73-434E-907D-61E3C3F063A4");
-BLECharacteristic mainCharacteristic("E03B819E-3A13-405D-A6BA-2F4D6523AC28", BLERead | BLEWrite | BLEIndicate | BLENotify, 512, true);
+BLECharacteristic mainCharacteristic("E03B819E-3A13-405D-A6BA-2F4D6523AC28", BLERead | BLEWrite | BLEIndicate | BLENotify | BLEWriteWithoutResponse, 40, true);
 
 
 Custom_Robot_Control::Custom_Robot_Control()
@@ -18,8 +21,6 @@ Custom_Robot_Control::Custom_Robot_Control()
 
 bool Custom_Robot_Control::begin()
 {
-  pinMode(_pin, OUTPUT);
-
   if (!BLE.begin()) {
       return false;
   }
@@ -27,7 +28,7 @@ bool Custom_Robot_Control::begin()
   BLE.setAdvertisedService(mainService);
   mainService.addCharacteristic(mainCharacteristic);
   BLE.addService(mainService);
-  if(!mainCharacteristic.writeValue((const byte*)intArray, 512)) return false;
+  if(!mainCharacteristic.writeValue((const byte*)longArray, 40)) return false;
   if(!BLE.advertise()) return false;
   delay(800);
   return true;
@@ -46,43 +47,43 @@ bool Custom_Robot_Control::isConnected() {
 
 bool Custom_Robot_Control::readBuffer()
 {
-    if (mainCharacteristic.readValue((void*)(const byte*)intArray, 512) != 512) return false;
+    if (mainCharacteristic.readValue((void*)(const byte*)longArray, 40) != 40) return false;
     return true;
 }
 
 bool Custom_Robot_Control::writeBuffer()
 {
-    if (!mainCharacteristic.writeValue((const byte*)intArray, 512)) return false;
+    if (!mainCharacteristic.writeValue((const byte*)longArray, 40)) return false;
     return true;
 }
 
-bool Custom_Robot_Control::write(int index, int value)
+bool Custom_Robot_Control::write(long index, long value)
 {
     if (index < 0) return false;
-    if (index >= 128) return false;
+    if (index >= 10) return false;
     if (!readBuffer()) return false;
-    intArray[index] = value;
+    longArray[index] = value;
     if (!writeBuffer()) return false;
     return true;
 }
 
 // reads from a location (index) and returns -1 for errors (note that -1 can be written to the location, so it does not always signal an error)
-int Custom_Robot_Control::read(int index)
+long Custom_Robot_Control::read(long index)
 {
     if (index < 0) return -1;
-    if (index >= 128) return -1;
+    if (index >= 10) return -1;
     if (!readBuffer()) return -1;
-    return intArray[index];
+    return longArray[index];
 }
 
 // Returns whether a new value has been written (by either client or server) since the last call to this function.
 // Writing the same value again is not considered a write. Errors return false. 
-bool Custom_Robot_Control::isUpdated(int index)
+bool Custom_Robot_Control::isUpdated(long index)
 {
     if (index < 0) return false;
-    if (index >= 128) return false;
+    if (index >= 10) return false;
     if (!readBuffer()) return false;
-    bool written = (intArray[index] != intArrayWritten[index]);
-    intArrayWritten[index] = intArray[index];
+    bool written = (longArray[index] != longArrayWritten[index]);
+    longArrayWritten[index] = longArray[index];
     return written;
 }
